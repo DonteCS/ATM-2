73314680929a690fcedf7e9e6fc2d8225c3cec30
Donte <02donte.rious@gulllakecs.org>
2021-11-02 12:45:42 -0400

    Set up a pin for the users to have a secure account

--- ATM.py
+++ ATM.py
@@ -13,3 +13,17 @@ first_name = input("What is your first name: ")
 last_name = input("What is your last name: ")
 
 print("\nWelcome to Cash-R-Us", first_name, last_name + ", we will now set up a security pin on your account.\n")
+
+# Set up a pin for the users to have a secure account
+
+pin= input("please choose a 4-digit personal identification number:")
+
+print("\nThank you",first_name + ", we see that you set your PIN to",pin)
+
+print("\nWould you like to make a transaction through our automated teller machine")
+
+atm = input("Yes or no: "). lower()
+
+print(atm)
+if atm == "yes":
+   print("\n************************************************************************\n")
